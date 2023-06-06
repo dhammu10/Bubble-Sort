9178463d13e5de48319498ba935345402e077739
Dhammdip Modak <124656648+dhammu10@users.noreply.github.com>
2023-06-06 09:54:11 +0530

    Create BubbleSort.java
    Data Structure algorithm

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void BubSort(int arr[], int n){
+        int temp;
+        for(int i = n-1; i>=0; i--){
+            for (int j =0; j<i; j++){
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i=0; i<n; i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        BubSort(arr,n);
+
+    }
+}
